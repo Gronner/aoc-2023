4f842c586478e2854adeeff81d3805afb999d141
Gronner <felix.braeunling@t-online.de>
2023-12-01 09:25:36 +0100

    Update day 1 solution

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,8 +2,6 @@ use aoc_downloader::download_day;
 
 const DAY: u32 = 1;
 
-const WORDS: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
 fn get_input() -> Vec<String> {
     use std::io::BufRead;
     download_day(DAY, "input").unwrap();
@@ -23,28 +21,14 @@ pub fn run_day() {
     println!(
         "Running day {}:\n\tPart1 {}\n\tPart2 {}",
         DAY,
-        0, //part1(&input),
+        part1(&input),
         part2(&input)
     );
 }
 
-fn part1(input: &Vec<String>) -> u32 {
-    let mut output = Vec::new();
-    for line in input {
-        output.push(line.chars()
-            .filter_map(|c| c.to_digit(10))
-            .collect::<Vec<u32>>());
-    }
-    let mut sum = 0;
-    for set in output{
-        sum += set.first().unwrap() * 10 + set.last().unwrap();
-    }
-    sum
-}
-
-fn part2(input: &Vec<String>) -> u32 {
+fn calculate(input: &[String], replace: bool) -> u32 {
     let output = input.iter()
-        .map(|l| {
+        .map(|l| { if replace {
             l.replace("one", "o1e")
             .replace("two", "t2o")
             .replace("three", "t3e")
@@ -54,18 +38,25 @@ fn part2(input: &Vec<String>) -> u32 {
             .replace("seven", "s7n")
             .replace("eight", "e8t")
             .replace("nine", "n9e")
-            }
+            } else {
+                l.to_string()
+            }}
         )
         .map(|l| l.chars()
             .filter_map(|c| c.to_digit(10))
             .collect::<Vec<u32>>()
         ).collect::<Vec<_>>();
-    let mut sum = 0;
-    for set in output{
-        println!("{:?}", set);
-        sum += set.first().unwrap() * 10 + set.last().unwrap();
-    }
-    sum
+    output.iter()
+        .map(|s| s.first().unwrap() * 10 + s.last().unwrap())
+        .sum()
+}
+
+fn part1(input: &[String]) -> u32 {
+    calculate(input, false)
+}
+
+fn part2(input: &[String]) -> u32 {
+    calculate(input, true)
 }
 
 #[cfg(test)]
@@ -75,12 +66,12 @@ mod tests {
     #[test]
     fn day0_part1_output() {
         let input = parse_input(get_input());
-        assert_eq!(71502, part1(&input));
+        assert_eq!(54390, part1(&input));
     }
 
     #[test]
     fn day0_part2_output() {
         let input = parse_input(get_input());
-        assert_eq!(208191, part2(&input));
+        assert_eq!(54277, part2(&input));
     }
 }
