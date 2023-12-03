b3668df134dd097b140af842e15e0f89749ad715
Gronner <felix.braeunling@t-online.de>
2023-12-03 17:59:33 +0100

    Redo day 3 solution

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -13,10 +13,57 @@ fn get_input() -> Vec<String> {
     reader.lines().collect::<Result<_, _>>().unwrap()
 }
 
+fn read_schematic(schematic: &Vec<Vec<char>>) -> (Vec<Number>, Vec<Part>) {
+    let mut numbers = Vec::new();
+    let mut parts = Vec::new();
+
+    for (r, row) in schematic.iter().enumerate() {
+        let mut number = String::new();
+        let mut positions = HashSet::new();
+        for (c, col) in row.iter().enumerate() {
+            if col.is_ascii_digit() {
+                number.push(*col);
+                positions.insert((r, c));
+            } else {
+                if *col != '.' {
+                    parts.push(Part { symbol: *col, position: (r, c) });
+                }
+                if !number.is_empty() {
+                    numbers.push(Number {num: number.parse().unwrap(), positions: positions.clone()});
+                    number.clear();
+                    positions.clear();
+                }
+            }
+        }
+        if !number.is_empty() {
+            numbers.push(Number {num: number.parse().unwrap(), positions: positions.clone()});
+            number.clear();
+            positions.clear();
+        }
+    }
+
+    (numbers, parts)
+}
+
+fn relate_part_numbers(numbers: &Vec<Number>, parts: &Vec<Part>, dimensions: Pos) -> HashMap<Part, Vec<u32>> {
+    let mut part_mapping =  HashMap::new();
+    for part in parts {
+        for number in numbers {
+            if part.is_part_number(&number, dimensions) {
+                part_mapping
+                    .entry(*part)
+                    .and_modify(|m: &mut Vec<u32>| m.push(number.num))
+                    .or_insert(vec![number.num]);
+            }
+        }
+    }
+    part_mapping
+}
+
 fn parse_input(input: Vec<String>) -> HashMap<Part, Vec<u32>> {
     let input = input.iter().map(|l| l.chars().collect()).collect();
-    let numbers = extract_numbers(&input);
-    map_parts_to_number(&input, &numbers)
+    let (numbers, parts) = read_schematic(&input);
+    relate_part_numbers(&numbers, &parts, (input.len(), input[0].len()))
 }
 
 pub fn run_day() {
@@ -29,45 +76,12 @@ pub fn run_day() {
         part2(&input)
     );
 }
-
-fn get_parts(positions: &Vec<Pos>, input: &Vec<Vec<char>>) -> HashSet<Part> {
-    const OFFSETS: [(isize, isize); 8] = [
-        (-1, 1),
-        (0, 1),
-        (1, 1),
-        (-1, 0),
-        (1, 0),
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-    ];
-    let mut parts = HashSet::new();
-    for position in positions {
-        for offset in OFFSETS {
-            let next_row = position.0 as isize + offset.0;
-            let next_col = position.1 as isize + offset.1;
-            if next_row < 0
-                || next_row >= input.len() as isize
-                || next_col < 0
-                || next_col >= input[0].len() as isize
-            {
-                continue;
-            }
-            let symbol = input[next_row as usize][next_col as usize];
-            if !(symbol.is_ascii_digit() || symbol == '.') {
-                parts.insert(Part { symbol, position: (next_row as usize, next_col as usize) });
-            }
-        }
-    }
-    parts
-}
-
 type Pos = (usize, usize);
 
 #[derive(Debug)]
 struct Number {
-    num: u32,
-    positions: Vec<Pos>,
+    pub num: u32,
+    pub positions: HashSet<Pos>,
 }
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
@@ -76,50 +90,28 @@ struct Part {
     position: Pos,
 }
 
-fn extract_numbers(input: &Vec<Vec<char>>) -> Vec<Number> {
-    let mut numbers = Vec::new();
-    for (r, row) in input.iter().enumerate() {
-        let mut number = String::new();
-        let mut positions = Vec::new();
-        for (c, col) in row.iter().enumerate() {
-            if col.is_ascii_digit() {
-                number.push(*col);
-                positions.push((r, c));
-            } else if !number.is_empty() {
-                numbers.push(Number {
-                    num: number.parse().unwrap(),
-                    positions: positions.clone(),
-                });
-                number.clear();
-                positions.clear();
+impl Part { 
+    fn is_part_number(&self, number: &Number, dimensions: Pos) -> bool {
+        const OFFSETS: [(isize, isize); 8] = [
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+            (-1, 0),
+            (1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+        ];
+        let mut adjacent = HashSet::new();
+        for offset in OFFSETS {
+            let next_row = self.position.0.checked_add_signed(offset.0);
+            let next_col = self.position.1.checked_add_signed(offset.1);
+            if next_row.is_some_and(|r| r < dimensions.0) && next_col.is_some_and(|c| c < dimensions.1) {
+                adjacent.insert((next_row.unwrap(), next_col.unwrap()));
             }
         }
-        if !number.is_empty() {
-            numbers.push(Number {
-                num: number.parse().unwrap(),
-                positions: positions.clone(),
-            });
-            number.clear();
-            positions.clear();
-        }
+        !number.positions.is_disjoint(&adjacent)
     }
-    numbers
-}
-
-fn map_parts_to_number(
-    input: &Vec<Vec<char>>,
-    numbers: &Vec<Number>,
-) -> HashMap<Part, Vec<u32>> {
-    let mut part_mapping = HashMap::new();
-    for number in numbers {
-        for p in get_parts(&number.positions, input) {
-            part_mapping
-                .entry(p)
-                .and_modify(|m: &mut Vec<u32>| m.push(number.num))
-                .or_insert(vec![number.num]);
-        }
-    }
-    part_mapping
 }
 
 fn part1(input: &HashMap<Part, Vec<u32>>) -> u32 {
