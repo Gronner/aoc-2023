836c5d84440f7aa9bbd14f6e51be32996b0e9273
Gronner <felix.braeunling@t-online.de>
2023-12-14 19:28:41 +0100

    Add day 14 solution

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -32,5 +33,6 @@ pub fn get_days() -> Vec<fn()> {
         day11::run_day,
         day12::run_day,
         day13::run_day,
+        day14::run_day,
     ]
 }
